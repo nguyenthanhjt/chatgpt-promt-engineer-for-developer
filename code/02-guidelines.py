ce145a2435136715a16cf38bfcf3519a145dc205
Thanh Bui Nguyen <nguyenthanhjt@gmail.com>
2024-08-12 02:01:53 +0700

    update section 2 code

--- code/02-guidelines.py
+++ code/02-guidelines.py
@@ -5,6 +5,7 @@ import os
 # _ = load_dotenv(find_dotenv()) # read local .env file
 
 # openai.api_key = os.getenv('OPEN_API_KEY')
+# To get OpenAI API Key: Access to https://platform.openai.com/settings/profile?tab=api-keys
 openai.api_key = "sk-"
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
@@ -16,7 +17,9 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     )
     return response.choices[0].message["content"]
 
-# Tactic 1: Use delimiters
+# Principle 1: Write clear and specific Instructions
+
+## Tactic 1: Use delimiters
 text = f"""
 You should express what you want a model to do by 
 providing instructions that are as clear and 
@@ -43,7 +46,9 @@ Generate a list of three made-up book titles along with their authors and genres
 response = get_completion(prompt)
 print(response)
 
-# Tactic 2: Ask for structured output:
+## Tactic 2: Ask for structured output:
+
+### Example 1
 text_1 = f"""
 Making a cup of tea is easy! First, you need to get some water boiling. While that's happening, cup and put a tea bag in it. Once the water is grab
 hot enough, just pour it over the tea bag. Let it sit for a bit so the tea can steep. After a few minutes, take out the tea bag. If you
@@ -69,8 +74,26 @@ If the text does not contain a sequence of instructions, chen simply write \"No 
 
 response = get_completion(prompt)
 print ("Completion for Text 1:")
-
 print (response)
+
+### Example 2
+
+prompt = f"""
+Your task is to answer in  a consistent style.
+
+<child>: Teach me about patience.
+
+‹grandparent>: The river that carves the deepest valley flows from a modest spring; the grandest symphony originates a from a single note;
+the most intricate tapestry begins with a solitary thread.
+
+<child>: Teach me about resilience. Y
+"""
+
+response = get_completion(prompt)
+print ("Completion for Few-shot prompting example:")
+print (response)
+
+
 
 text_2 = f"""
 The sun is shining brightly today, and the birds are singing. It's a beautiful day to go for a
@@ -78,3 +101,171 @@ walk in the park. The flowers are blooming, and the trees are swaying gently in 
 are out and about, enjoying the lovely weather. Some are having picnics, while others are playing
 games or simply relaxing on the grass. It's a perfect day to spend time outdoors and appreciate the beauty of nature.
 """
+
+## tactic 4: few-shot prompting
+
+prompt = f"""
+Your task is to answer in d consistent style.
+
+<child>: Teach me about patience.
+
+‹grandparent>: The river that carves the deepest valley flows from a modest spring; the grandest symphony originates a from a single note;
+the most intricate tapestry begins with a solitary thread.
+
+<child>: Teach me about resilience. Y
+"""
+
+response = get_completion(prompt)
+print ("Completion for Few-shot prompting example:")
+print (response)
+
+# Principle 2: Give the model time to think
+
+## Tactic 1: specify the steps to complete a task
+### example 1
+text = f"""
+In a charming village, siblings Jack and Jill set out on quest to fetch water from a hilltop well.
+As they climbed, singing joyfully, misfortune struck-Jack tripped on stone and tumbled down the hill, with Jill following suit.
+Though slightly battered, the pair returned home to comforting embraces. 
+Despite the mishap, their adventurous spirits remained undimmed, and they continued exploring with delight.
+"""
+
+prompt_1 = f"""
+Your task is to perform the following actions:
+1 - Summarize the following text delimited by triple backticks with 1 sentence.
+2 - Translate the summary into French
+3 - List each name in the French summary
+4 - Output a json object that contains the following keys: french_summary, num_names.
+
+Separate your answer with line breaks.
+
+Text:
+```{text}````
+"""
+
+response = get_completion(prompt_1)
+print ("Completion for prompt_1:")
+print (response)
+
+### example 2: asking for output in a specified format
+text = f"""
+In a charming village, siblings Jack and Jill set out on quest to fetch water from a hilltop well.
+As they climbed, singing joyfully, misfortune struck-Jack tripped on stone and tumbled down the hill, with Jill following suit.
+Though slightly battered, the pair returned home to comforting embraces. 
+Despite the mishap, their adventurous spirits remained undimmed, and they continued exploring with delight.
+"""
+
+prompt_2 = f"""
+Your task is to perform the following actions:
+1 - Summarize the following text delimited by <> with 1 sentence.
+2 - Translate the summary into French
+3 - List each name in the French summary
+4 - Output a json object that contains the following keys: french_summary, num_names.
+
+Use the following format:
+Text: <text to summarize>
+Summary: <summary
+Translation: <summary translation
+Names: <List of names in Italian summary>
+Output JSON: <json with summary and num_names>
+
+Text to summarize:<{text}>
+"""
+
+response = get_completion(prompt_2)
+print ("Completion for prompt_2")
+print (response)
+
+## Tactic 2: Instruct the model to work out its own solution before rushing to a conclusion
+
+### Princle 2 - Tactic 2 - Example 1
+prompt = f"""
+Determine if the student's solution is correct or not.
+
+Question:
+I'm building a solar power installation and I need help working out the financials.
+- Land costs $100 / square foot
+- I can buy solar panels for $250 / square foot
+- I negotiated a contract for maintenance that will cost me a flat $100k per year, and an additional $10 / square foot
+What is the total cost for the first year of operations as a function of the number of square feet?
+
+
+Student's Solution:
+Let x be the size of the installation in square feet.
+Costs:
+1. Land cost: 100x
+2. Solar panel cost: 250x
+3. Maintenance cost: 100,000 + 100X
+Total cost: 100x + 250x + 100,000 + 100x = 450x + 100,000
+"""
+response = get_completion(prompt)
+print (response) 
+
+### Princle 2 - Tactic 2 - Example 2
+
+prompt = f"""
+Your task is to determine if the student's solution is correct or not.
+To solve the problem do the following: 
+- First, work out your own solution to the problem.
+- Then compare your solution to the student's solution and evaluate if the student's solution is correct or not.
+Don't decide if the student's solution is correct until you have done the problem yourself.
+
+Use the following format:
+Question:
+```
+question here
+```
+
+Student's solution:
+```
+student's solution here
+```
+
+Actual solution:
+```
+steps to work out the solution and your solution here
+```
+
+Is the student's solution the same as actual solution just calculated:
+```
+yes or no
+```
+
+Student grade:
+```
+correct or incorrect
+```
+
+Question:
+```
+Question:
+I'm building a solar power installation and I need help working out the financials.
+- Land costs $100 / square foot
+- I can buy solar panels for $250 / square foot
+- I negotiated a contract for maintenance that will cost me a flat $100k per year, and an additional $10 / square foot
+What is the total cost for the first year of operations as a function of the number of square feet?
+```
+
+Student's Solution:
+```
+Let x be the size of the installation in square feet.
+Costs:
+1. Land cost: 100x
+2. Solar panel cost: 250x
+3. Maintenance cost: 100,000 + 100X
+Total cost: 100x + 250x + 100,000 + 100x = 450x + 100,000
+```
+
+Actual Solution:
+"""
+response = get_completion(prompt)
+print (response) 
+
+# Model Limiations
+
+## example 1
+prompt = f"""
+Tell me about AeroGlide UltraSlim Smart Toothbrush by Boie
+"""
+response = get_completion(prompt)
+print (response) 
